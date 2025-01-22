138453aae41bb63f1b992f3c4405a8085c90eb9e
Lucas Simas <simaslucas1m@gmail.com>
2025-01-22 19:27:05 -0300

    Retirando o Texto do cabeçario

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const blocoPresente = document.querySelector(".bloco-presente")
 const presente = document.querySelector(".presente")
 const presenteAberto = document.querySelector(".presente-aberto")
 const carta = document.getElementById("carta")
-
+const headerText = document.getElementById("headerText")
 
 blocoPresente.addEventListener('click', () =>{
     presente.style.display = "none"
     presenteAberto.style.display = "block"
     carta.style.display = "block"
+    headerText.style.display = "none"
 })
